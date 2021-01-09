06e5af0398de61dff65a7e966bfabaac1f4f8755
Megan Grosz <megrosz@gmail.com>
2021-01-09 16:48:22 +0100

    started edit task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -19,6 +19,17 @@ class TasksController < ApplicationController
     redirect_to task_path(@task)
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(params[:task])
+
+    redirect_to task_path(@task)
+  end
+
   private
 
   def task_params
